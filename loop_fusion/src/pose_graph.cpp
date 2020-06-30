d28203a5e3a43f919abf56ff3627576d21d83b42
Javier Cremona <cremona@cifasis-conicet.gov.ar>
2020-06-30 14:44:54 -0300

    Change format for vio_loop.csv

--- loop_fusion/src/pose_graph.cpp
+++ loop_fusion/src/pose_graph.cpp
@@ -830,16 +830,16 @@ void PoseGraph::updatePath()
         {
             ofstream loop_path_file(VINS_RESULT_PATH, ios::app);
             loop_path_file.setf(ios::fixed, ios::floatfield);
-            loop_path_file.precision(0);
-            loop_path_file << (*it)->time_stamp * 1e9 << ",";
+            // loop_path_file.precision(0);
+            loop_path_file << (*it)->time_stamp << " ";
             loop_path_file.precision(5);
-            loop_path_file  << P.x() << ","
-                  << P.y() << ","
-                  << P.z() << ","
-                  << Q.w() << ","
-                  << Q.x() << ","
-                  << Q.y() << ","
-                  << Q.z() << ","
+            loop_path_file  << P.x() << " "
+                  << P.y() << " "
+                  << P.z() << " "
+                  << Q.w() << " "
+                  << Q.x() << " "
+                  << Q.y() << " "
+                  << Q.z()
                   << endl;
             loop_path_file.close();
         }
